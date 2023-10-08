2bee0d3c4cf909787eb2423b24d441aaab16a8e6
bellh14 <haybell965@gmail.com>
2023-10-08 14:37:59 -0500

    Add drag analysis data

--- AeroDataAnalysis.py
+++ AeroDataAnalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from typing import Final
 from Visualizer import Visualizer
+from sklearn.preprocessing import Normalizer, MinMaxScaler
 
 
 class AeroDataAnalysis:
@@ -13,6 +14,9 @@ class AeroDataAnalysis:
         self.base_file_name = base_file_name
         self.folder = folder_name
         self.aero_data = pd.DataFrame()
+
+    def load_data(self, file_name: str):
+        self.aero_data = pd.DataFrame(pd.read_csv(f"{self.folder}{file_name}"))
 
     def merge_csv_data(self, output_file: str) -> pd.DataFrame:
 
@@ -29,6 +33,7 @@ class AeroDataAnalysis:
         return merged_data
 
     def create_aeromap_df(self, output_file: str,
+                          target_column: str = "Raw Downforce Mean",
                           save_csv: bool = False) -> pd.DataFrame:
 
         aeromap_df = pd.DataFrame()
@@ -39,7 +44,7 @@ class AeroDataAnalysis:
             if i == 0:
                 aeromap_df = pd.concat([aeromap_df, data])
 
-            for j in range(round(data.loc[i, "Raw Downforce Mean"])):
+            for j in range(round(data.loc[i, target_column])):
                 aeromap_df = pd.concat([aeromap_df, data])
 
         if save_csv:
@@ -47,11 +52,39 @@ class AeroDataAnalysis:
 
         return aeromap_df
 
+    def save_selected_columns(self, columns: list, output_file: str) -> None:
+        self.aero_data[columns].to_csv(f"{self.folder}{output_file}",
+                                       index=False)
+
+    def normalize_columns(self, columns: list, save_csv: False,
+                          output_file: str = None) -> None:
+        # normalizer = Normalizer()
+        # self.aero_data[columns] = normalizer.fit_transform(
+        #     self.aero_data[columns])
+        scaled = MinMaxScaler()
+        self.aero_data[columns] = scaled.fit_transform(
+            self.aero_data[columns])
+
+        if save_csv:
+            self.aero_data.to_csv(f"{self.folder}{output_file}",
+                                  index=False)
+
 
 if __name__ == "__main__":
-    file_name = "2024DesignStint4BaselineBullhornsBeamWingCleared_Report.csv"
-    aeromap = AeroDataAnalysis(file_name, "2024V4/")
-    aeromap.aero_data = aeromap.merge_csv_data("2024_aeromap_v4.csv")
-    aeromap.create_aeromap_df("2024_aeromap_contour_v4.csv")
-    visualizer = Visualizer(aeromap.aero_data)
-    visualizer.plot_aeromap()
+    # file_name = "2024DesignStint4BaselineBullhornsBeamWingCleared_Report.csv"
+    file_name = "2024_aeromap.csv"
+    aeromap = AeroDataAnalysis(file_name, "2024AeroMapV1/")
+    aeromap.load_data(file_name)
+    aeromap.save_selected_columns(["Front Rideheight", "Rear Rideheight",
+                                   "Raw Downforce Mean", "Raw Drag Mean"],
+                                  "2024_v1_drag_data.csv")
+    aeromap.load_data("2024_v1_drag_data.csv")
+    aeromap.normalize_columns(["Raw Downforce Mean", "Raw Drag Mean"],
+                              save_csv=True,
+                              output_file="2024_v1_drag_normalized.csv")
+
+    # aeromap.aero_data = aeromap.merge_csv_data("2024_aeromap_v4.csv")
+    # aeromap.create_aeromap_df("2023_aeromap_drag_contour.csv", "Raw Drag Mean",
+    #                           save_csv=True)
+    # visualizer = Visualizer(aeromap.aero_data)
+    # visualizer.plot_aeromap(target_column="Raw Drag Mean")

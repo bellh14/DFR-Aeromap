575233e9ef5331f47341a33169f8c5ee445aac5d
bellh14 <haybell965@gmail.com>
2023-11-07 09:36:44 -0600

    fix the secrets

--- batch_scripts/starccm-job-script_ganymede.sh
+++ batch_scripts/starccm-job-script_ganymede.sh
@@ -12,4 +12,4 @@ NCPU=80
 echo -e "This job allocated $NCPU cpu(s)\nJob is allocated on node(s): $SLURM_JOB_NODELIST" > /home/hxb210012/scratch/yaw_angle/batchlog.txt
 module load starccm/17.04.007
 
-starccm+ -power -licpath 1999@flex.cd-adapco.com -podkey VT45ervXkzkWL6cXrwhoEw -batch "/petastore/ganymede/home/hxb210012/yaw_angle/YawAngleDOE.java" -np $NCPU "/petastore/ganymede/home/hxb210012/yaw_angle/2024Baseline13in_Straightline_sprung.sim" -bs slurm -time -batch-report
+starccm+ -power -licpath 1999@flex.cd-adapco.com -podkey <podkey> -batch "/petastore/ganymede/home/hxb210012/yaw_angle/YawAngleDOE.java" -np $NCPU "/petastore/ganymede/home/hxb210012/yaw_angle/2024Baseline13in_Straightline_sprung.sim" -bs slurm -time -batch-report
